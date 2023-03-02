26530ad4db9798b2ede4353d782c5baf5407a4f6
Hacker God <jyrone.parker@gmail.com>
2023-03-02 15:58:59 -0500

    made prompt command

---
+++ commands/prompt.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+
+const { Configuration, OpenAIApi } = require("openai")
+const { SlashCommandBuilder } = require('discord.js');
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function generate(prompt, model="text-davinci-003") {
+  const completion = await openai.createCompletion({
+    model: model,
+    prompt: prompt
+  });
+  const text = completion.data.choices[0].text
+  return text;
+}
+module.exports = {
+	data: new SlashCommandBuilder()
+   .setName('generate-prompt')
+   .setDescription('Generate a ChatGPT reponse!')
+   .addStringOption(option =>
+     option
+     .setName('prompt')
+     .setDescription('The prompt to generate')
+     .setRequired(true)
+    ),
+
+	async execute(interaction) {
+    const res = await generate(interaction.options.getString('prompt'))
+		await interaction.reply(res);
+	},
+};
--- index.js
+++ index.js
@@ -1,21 +1,65 @@
 require('dotenv').config()
-const { Client } = require('discord.js');
+const { Client, Events, Collection, REST, Routes } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Configuration, OpenAIApi } = require("openai")
 const client = new Client({ intents: 2048 })
+client.commands = new Collection()
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function generate(prompt, model="text-davinci-003") {
+  const completion = await openai.createCompletion({
+    model: model,
+    prompt: prompt
+  });
+  const text = completion.data.choices[0].text
+  return text;
+}
+
 
 // start the discord client and listen for messages
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+const commands = []
+// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, process.env.DISCORD_GUILD_ID),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
 
 client.login(process.env.DISCORD_TOKEN)
- client.on('message', (message) => {
-   try {
-     if(message.author.bot) {
-       return;
-      } else {
-       /*
-        1) grab the message text
-        2) pass the text as a prompt to OpenAI
-       */
-     }
-   } catch (e) {
-     console.log(e.message)
-   }
- })
+ client.on('ready', async () => {
+   console.log('CONNECTED TO SERVER')
+   const channel = await client.channels.fetch(process.env.DISCORD_CHANNEL_ID)
+    })
+  client.on(Events.InteractionCreate, async interaction => {
+    if (interaction.commandName === 'generate-prompt') {
+      const res = await generate(interaction.options.getString('prompt'))
+      await interaction.reply({ content: res });
+    }
+    });
